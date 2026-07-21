8051dc7751a91e4a4c50ce094b778f3d42b7bfaa
Rolf <rolf.wiechert@roche.com>
2026-07-21 11:53:04 +0200

    check

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from 'next/link';
-import Image from 'next/image'
+//import Link from 'next/link';
+//import Image from 'next/image'
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
